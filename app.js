0da6ca4d0319934d9c2e36fc0302e4a91ee198cf
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-08-22 01:59:40 +0530

    start club-house project

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
